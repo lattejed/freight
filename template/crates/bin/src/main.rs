e5cf8df8e84be227e53ddb2154582b4be671d661
Matthew Wiriyathananon-Smith <m@lattejed.com>
2023-12-02 11:49:29 +0700

    feat: update templates

---
+++ template/crates/bin/src/main.rs
@@ -0,0 +1,23 @@
+// Copyright 2023-2023 <%[PROJECT_AUTHORS]%>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! <%[CRATE_NAME]%>
+
+#![doc = include_str!("../README.md")]
+#![warn(
+    missing_docs,
+    unreachable_pub,
+    non_ascii_idents,
+    unreachable_pub,
+    unused_crate_dependencies,
+    unused_results,
+    unused_qualifications,
+    nonstandard_style,
+    rustdoc::all
+)]
+#![deny(rust_2018_idioms, unsafe_code)]
